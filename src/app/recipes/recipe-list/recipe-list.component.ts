c65caaf27632415352c2a4e84cb37ba98d5e1d69
MoizAsif <moiz.asif@xynotech.com>
2022-07-01 15:59:18 +0500

    Adding Content to the Recipes Component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  //Recipe is basically coming from the recipe.model.ts
+  recipes: Recipe[] = [
+    //When we call the method like this we actually call the constructor
+    new Recipe('A Test Recipe', 'A description for a test recipe', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2017/6/10/0/FNM070117_Warm-Farro-Salad-with-Charred-Corn_s4x3.jpg.rend.hgtvcom.406.271.suffix/1497141547192.jpeg') 
+  ];
 
   constructor() { }
 
